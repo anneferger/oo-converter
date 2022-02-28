1528e3c9f0cbc2a7a14f09d5606d0025c2e0b028
Anne Ferger <anne.ferger@uni-paderborn.de>
2022-02-28 11:22:36 +0100

    example converter junit tests

---
+++ src/test/java/uk/ac/ox/oucs/oxgarage/oo/OOConverterTest.java
@@ -0,0 +1,53 @@
+package uk.ac.ox.oucs.oxgarage.oo;
+
+import pl.psnc.dl.ege.exception.ConverterException;
+import pl.psnc.dl.ege.types.ConversionActionArguments;
+import pl.psnc.dl.ege.types.DataType;
+
+import java.io.*;
+
+import static org.junit.Assert.*;
+
+public class OOConverterTest {
+    private OOConverter converter;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        converter = new OOConverter();
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+        converter = null;
+    }
+
+    @org.junit.Test
+    public void convert() throws IOException, ConverterException {
+        //text to pdf
+        InputStream is = new FileInputStream("src/test/resources/test-input.txt.zip");
+        OutputStream os = new FileOutputStream("src/test/resources/test-output.pdf.zip");
+        DataType inputType = new DataType("txt","text/plain");
+        DataType outputType = new DataType("pdf","application/pdf");
+        ConversionActionArguments conversionActionArguments = new ConversionActionArguments(inputType, outputType, null);
+        converter.convert(is, os, conversionActionArguments);
+        assertNotNull(new File("src/test/resources/test-output.pdf.zip"));
+        is.close();
+        os.close();
+        //docx to TEI
+        is = new FileInputStream("src/test/resources/test-input.docx.zip");
+        os = new FileOutputStream("src/test/resources/test-output.txt.zip");
+        inputType = new DataType("docx","application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        outputType = new DataType("txt","text/plain");
+        conversionActionArguments = new ConversionActionArguments(inputType, outputType, null);
+        converter.convert(is, os, conversionActionArguments);
+        assertNotNull(new File("src/test/resources/test-output.txt.zip"));
+        is.close();
+        os.close();
+    }
+
+    @org.junit.Test
+    public void getPossibleConversions() {
+        assertNotNull(converter.getPossibleConversions());
+        System.out.println(converter.getPossibleConversions());
+    }
+}
